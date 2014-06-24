af271fe578806e1ff97694d43ada314d61053ce0
Marcio Trindade <marciotrindade@gmail.com>
2014-06-24 00:51:18 -0300

    rename schema fields

--- schema.sql
+++ schema.sql
@@ -4,16 +4,15 @@
 DROP TABLE IF EXISTS "public"."logs";
 CREATE TABLE "logs" (
   "id"          serial,
-  "user"        text,
+  "username"    text,
   "database"    text,
   "duration"    numeric(10,3),
   "action"      text,
-  "table"       text,
+  "table_name"  text,
   "sql"         text,
   "created_at"  timestamp(6) NULL
-)
+);
 -- ----------------------------
 --  Primary key structure for table logs
 -- ----------------------------
 ALTER TABLE "logs" ADD PRIMARY KEY ("id") NOT DEFERRABLE INITIALLY IMMEDIATE;
-
