a9548333c84b50b9214ae9b48cd812bea8c4a7a5
Marcio Trindade <marciotrindade@gmail.com>
2014-07-14 23:37:27 -0300

    changed test to set query inside the it block

--- parser/copy_test.go
+++ parser/copy_test.go
@@ -10,25 +10,22 @@ import (
 var _ = Describe("Copy", func() {
 	Describe("TableOfCopy", func() {
 		Context("with schema name", func() {
-			query := "copy account_29512.import_19 (email,nome) from stdin (format 'csv')"
-
 			It("extracts table name of query", func() {
+				query := "copy account_29512.import_19 (email,nome) from stdin (format 'csv')"
 				Expect(TableOfCopy(query)).To(Equal("account_29512.import_19"))
 			})
 		})
 
 		Context("without schema name", func() {
-			query := "copy import_19 (email,nome) from stdin (format 'csv')"
-
 			It("extracts table name of query", func() {
+				query := "copy import_19 (email,nome) from stdin (format 'csv')"
 				Expect(TableOfCopy(query)).To(Equal("import_19"))
 			})
 		})
 
 		Context("when query is not a copy command", func() {
-			query := "select * from logs where duration > 1"
-
 			It("returns empty string", func() {
+				query := "select * from logs where duration > 1"
 				Expect(TableOfCopy(query)).To(BeEmpty())
 			})
 		})
