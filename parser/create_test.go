62f4210bd81cc34fb08fa108056b1d8c8a83cbfb
Marcio Trindade <marciotrindade@gmail.com>
2014-07-14 23:45:50 -0300

    changed test of create to use BeforeEach

--- parser/create_test.go
+++ parser/create_test.go
@@ -8,45 +8,71 @@ import (
 )
 
 var _ = Describe("Create", func() {
+	var (
+		query  string
+		action string
+		table  string
+	)
+
 	Describe("when create a table", func() {
 		Context("without schema name", func() {
 			Context("with schema name", func() {
-				query := "create table account_5943.import_5 (id serial primary key, email text, text, text, text, text)"
+				BeforeEach(func() {
+					query = "create table account_5943.import_5 (id serial primary key, email text, text, text, text, text)"
+					action, table = TableOfCreate(query)
+				})
+
+				It("extracts action name of query", func() {
+					Expect(action).To(Equal("create table"))
+				})
 
 				It("extracts table name of query", func() {
-					action, table := TableOfCreate(query)
-					Expect(action).To(Equal("create table"))
 					Expect(table).To(Equal("account_5943.import_5"))
 				})
 			})
 
 			Context("without schema name", func() {
-				query := "create table \"templates\" (\"id\" serial primary key, \"name\" character varying(255) not null, \"account_id\" integer, \"created_at\" timestamp not null, \"updated_at\" timestamp not null)"
+				BeforeEach(func() {
+					query = `create table "templates" ("id" serial primary key, "name" character varying(255) not null, "account_id" integer, "created_at" timestamp not null, "updated_at" timestamp not null)`
+					action, table = TableOfCreate(query)
+				})
+
+				It("extracts action name of query", func() {
+					Expect(action).To(Equal("create table"))
+				})
 
 				It("extracts table name of query", func() {
-					action, table := TableOfCreate(query)
-					Expect(action).To(Equal("create table"))
 					Expect(table).To(Equal("templates"))
 				})
 			})
 		})
 
 		Context("when create a index", func() {
-			query := "create  index \"index_openings_on_contact_id\" on \"openings\" (\"contact_id\")"
+			BeforeEach(func() {
+				query = `create  index "index_openings_on_contact_id" on "openings" ("contact_id")`
+				action, table = TableOfCreate(query)
+			})
+
+			It("extracts action name of query", func() {
+				Expect(action).To(Equal("create index"))
+			})
 
 			It("extracts table name of query", func() {
-				action, table := TableOfCreate(query)
-				Expect(action).To(Equal("create index"))
 				Expect(table).To(Equal("index_openings_on_contact_id"))
 			})
 		})
 
 		Context("when query is not a create command", func() {
-			query := "select * from logs where duration > 1"
+			BeforeEach(func() {
+				query = "select * from logs where duration > 1"
+				action, table = TableOfCreate(query)
+			})
 
-			It("returns empty string", func() {
-				action, table := TableOfCreate(query)
+			It("returns empty action", func() {
 				Expect(action).To(BeEmpty())
+			})
+
+			It("returns empty table", func() {
 				Expect(table).To(BeEmpty())
 			})
 		})
